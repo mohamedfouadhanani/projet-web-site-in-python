b84abc61d1354c69c879044817cae2730ac7636b
Mohamed Fouad HANANI <mohamedfouad.hanani@gmail.com>
2023-06-04 11:23:38 +0100

    registered yen bleuprint to flask app

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
 
 from routes.usd import usd_blueprint
+from routes.yen import yen_blueprint
 
 app = Flask(__name__)
 
 app.register_blueprint(usd_blueprint, url_prefix="/usd")
+app.register_blueprint(yen_blueprint, url_prefix="/yen")
 
 @app.route("/", methods=["GET"])
 def index():

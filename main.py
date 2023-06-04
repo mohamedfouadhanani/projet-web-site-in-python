310148cde2b3275797d30b180087248b3f4ec1eb
Mohamed Fouad HANANI <92801060+mohamedfouadhanani@users.noreply.github.com>
2023-06-04 11:08:13 +0100

    added dzd to usd (#1)

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template
 
+from routes.usd import usd_blueprint
+
 app = Flask(__name__)
+
+app.register_blueprint(usd_blueprint, url_prefix="/usd")
 
 @app.route("/", methods=["GET"])
 def index():
